69387298dbd9fdb82dd0e65ca22e6492960216f8
Sayed Mehrwarz <sayed.mehrwarz@alexandriava.gov>
2025-04-30 15:22:57 -0400

    drawing an svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ export default function Home() {
   return (
     <div className="main">
       <h1>Home page. </h1>
-    </div>
+      <svg height={300} width={360}>
+        <path stroke="bluw" fill="#2255bb" d="M 32,195 L 32,30 10,30 10,220  Q 50 230 80 240 110 250 150 270 190 300  190 275 
+          150 250 100 220 60 200 Z" />
+        {/* <path fill="green" stroke="green" d="M 30 50 Q 100 40 150 60 200 100 100 270" /> */}
+      </svg>
+
+    </div >
   );
 }
